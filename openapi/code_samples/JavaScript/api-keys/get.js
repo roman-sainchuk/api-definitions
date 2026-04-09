9dad835e224743e23ae6c878a8faa6d1a298f09f
Roman Sainchuk <roman.sainchuk@redocly.com>
2026-04-09 09:20:10 +0000

    add console.,lof

--- openapi/code_samples/JavaScript/api-keys/get.js
+++ openapi/code_samples/JavaScript/api-keys/get.js
@@ -1,6 +1,8 @@
 // all parameters are optional
 const firstCollection = await api.apiKeys.getAll();
 
+console.log(firstCollection);
+
 // alternatively you can specify one or more of them
 const params = {limit: 20, offset: 100, sort: '-createdTime'};
 const secondCollection = await api.apiKeys.getAll(params);
